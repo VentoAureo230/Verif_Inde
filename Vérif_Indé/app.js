80f8a71719a21421822e3393e7428b572f5001be
VentoAureo230 <anthony.meny35@gmail.com>
2022-09-05 16:20:00 +0200

    App.js avec node de préparé

--- Vérif_Indé/app.js
+++ Vérif_Indé/app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req,res) => {
-    res.send('Hello World')
+app.get('/', (req, res) => {
+  res.send('Hello World! je suis Anthony')
 })
 
 app.listen(port, () => {
-    console.log(app, {3000})
+  console.log()
 })
