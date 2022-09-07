7568f170a7b0008e839d080d8c8fde5797b15557
Bastienlsg <95766791+Bastienlsg@users.noreply.github.com>
2022-09-07 10:19:53 +0200

    nouveauté js

--- Vérif_Indé/app_2.0.js
+++ Vérif_Indé/app_2.0.js
@@ -1,31 +1,27 @@
 let root = document.documentElement;
 
-window.onload = resetNeedle();
-
-function resetNeedle()
-{
-    //aiguille.setAttribute("transform","rotate(-138)")
-    var aiguille = document.getElementById('aiguille');
-    aiguille.style.animation = 'none';
-    aiguille.offsetHeight;
-    aiguille.style.animation = null;
-}
-
 function Btn_1Click()
 {
     var aiguille = document.getElementById("aiguille");
-    resetNeedle();
     var angle = 1;
     var score = Math.floor( Math.random() *100 ) ;
-    console.log(score)
     angle = ( 178 * (score / 100) ) - 138 ;
     //root.style.setProperty('--angle',angle);
     
     root.style.setProperty('--angle4', ((score / 100) * 178 ) - 138 );
     aiguille.style.animationPlayState = "running";
 
-    
-
-
-    //aiguille.setAttribute("transform", `rotate(${angle})`);
+    /*if (0 < score < 25) {
+        const pNode = document.getElementById("p");
+        pNode.textContent = "Cet article est fake, fuyez.";
+    } else if (25 < score < 50) {
+        const pNode = document.getElementById("p");
+        pNode.textContent = "Cet article est douteux, faites attention.";
+    } else if (50 < score < 75) {
+        const pNode = document.getElementById("p");
+        pNode.textContent = "Cet article est plutôt honnête.";
+    } else if (75 < score < 100) {
+        const pNode = document.getElementById("p");
+        pNode.textContent = "Cet article est honnête.";
+    }*/
 }
